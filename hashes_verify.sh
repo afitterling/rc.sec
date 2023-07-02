3abbad80d0babe46b4a9e34572d017e50805731b
Alexander Fitterling <info@sp33c.tech>
2023-07-02 12:40:17 +0200

    fixed

--- hashes_verify.sh
+++ hashes_verify.sh
@@ -6,8 +6,13 @@ find $toverifydir -type f |
 while read each; do
 	regexp="s/$(echo $toverifydir | sed -e "s/\//\\\\\//g")//"
 	file=$(echo "$each" | sed "$regexp")
-	hash=`cat "$chksumdir/$file.sum" | awk '{ print $1 }'`
-        #echo "$hash $toverifydir/$file" | sha512sum -c 
-        echo "$hash $toverifydir/$file" | sha512sum -c --quiet 
+	test -f "$chksumdir/$file" && {
+	  hash=`cat "$chksumdir/$file.sum" | awk '{ print $1 }'`
+          #echo "$hash $toverifydir/$file" | sha512sum -c 
+          echo "$hash $toverifydir/$file" | sha512sum -c --quiet 
+	}
+	test -f "$chksumdir/$file" || {
+	  echo "warning, untracked file: $toverifydir$file"
+	}
 done
 
