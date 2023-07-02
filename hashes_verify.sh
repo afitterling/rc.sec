b29b3c4050da56c2b7f154ed851f1f12dd131d08
Alex Fitterling <info@sp33c.tech>
2023-07-02 12:31:46 +0200

    Update hashes_verify.sh

--- hashes_verify.sh
+++ hashes_verify.sh
@@ -6,12 +6,8 @@ find $toverifydir -type f |
 while read each; do
 	regexp="s/$(echo $toverifydir | sed -e "s/\//\\\\\//g")//"
 	file=$(echo "$each" | sed "$regexp")
-	#test -e $chksumdir/$file.sum && { 
-		hash=`cat "$chksumdir/$file.sum" | awk '{ print $1 }'`
-	        #echo "$hash $toverifydir/$file" | sha512sum -c 
-	        echo "$hash $toverifydir/$file" | sha512sum -c --quiet 
-	#} || {
-	#	echo "warning: file without checksum: $toverifydir$file"
-	#}
+	hash=`cat "$chksumdir/$file.sum" | awk '{ print $1 }'`
+        #echo "$hash $toverifydir/$file" | sha512sum -c 
+        echo "$hash $toverifydir/$file" | sha512sum -c --quiet 
 done
 
